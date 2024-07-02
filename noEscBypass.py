6d9096437b021c2b912b6bc59b5b42fd808cafc1
n0ps <68410650+n0psn0ps@users.noreply.github.com>
2024-07-01 17:51:42 -0700

    Update noEscBypass.py
    Script update

--- noEscBypass.py
+++ noEscBypass.py
@@ -4,21 +4,27 @@ import time
 # launch the ios application
 r = r2pipe.open("frida://spawn/usb//com.mobilehackinglab.No-Escape")
 
-# mov overwrite
-print("## Bypassing mov instructions")
-r.cmd('s `:iE~+jail[0]`');
-mov = r.cmd('pd~mov[1]');
-arMov = mov.split();
-r.cmd('s ' + arMov[0] + '; wx 00008052; s ' + arMov[1] + '; wx 00008052; s ' + arMov[2] + '; wx 00008052');
+def patch_instruction(address):
+    r.cmd(f"s {address}")
+    r.cmd("wa mov w0, 0")
 
-# tbz overwrite  
-print("## Bypassing tbz instructions")
-r.cmd('s `:iE~+jail[0]`');
-tbz = r.cmd('pd~tbz[1]');
-arTbz = tbz.split();
-r.cmd('s ' + arTbz[0] + '; wx 00008052; s ' + arTbz[1] + '; wx 00008052; s ' + arTbz[2] + '; wx 00008052'+ arTbz[3] + '; wx 00008052');
+print("## Bypassing mov & tbz instructions")
+
+mov_output = r.cmd('s `:iE~+jail[0]`; pd~mov[1]]').splitlines()
+mov_addresses = [line.split()[0] for line in mov_output]
+
+tbz_output = r.cmd('s `:iE~+jail[0]`; pd~tbz[1]').splitlines()
+tbz_addresses = [line.split()[0] for line in tbz_output]
+# Patch 'tbz' instructions
+for addr in tbz_addresses:
+    patch_instruction(addr)
+
+# Patch 'mov w0, 1' instructions
+for addr in mov_addresses:
+    patch_instruction(addr)
 
 # continue
+print("No Escape successfully bypassed")
 r.cmd(':dc')
 
 # sleep the application
