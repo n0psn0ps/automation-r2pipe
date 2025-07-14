c8f881aa5a6f2507901a34e2259a6db3208354dc
n0ps <68410650+n0psn0ps@users.noreply.github.com>
2025-07-13 21:08:51 -0700

    Update fitmPatch03.py

--- fitmPatch03.py
+++ fitmPatch03.py
@@ -1,19 +1,18 @@
-# patch 2
+#patch 2
 import r2pipe
 import time
 
 # launch the android application
 r = r2pipe.open("frida://spawn/usb//com.8ksec.FridaInTheMiddle")
 
-# seek to func location - emulate - analyze func
-r.cmd('s `:il~+debug[0]`; s `:iE~+canary[0]`; e anal.slow = false; e anal.nopskip = true; e emu.str = true; afr.; afna.')
+# split output and store in array
+add_output = r.cmd('s `:il~+debug[0]`;s `:iE~+canary[0]`; e anal.slow = false; e anal.nopskip = true; e emu.str = true; afr.; afna.;pdr.~+add[1]').splitlines()
 
-# sleep 1
+# sleep 
 time.sleep(2)
 
-# locate tbz instruction and nop
-r.cmd('wao nop @ `pdr.~+tbz[1]`')
-r.cmd(':dc')
+# overwrite second add instruction
+r.cmd('wao nop @ ' + add_output[2] +';:dc')
 
 # sleep the application
 time.sleep(10000)
